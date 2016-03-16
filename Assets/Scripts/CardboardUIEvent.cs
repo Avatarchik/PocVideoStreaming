6a3216ffd5562d522a93a9c1af92c2ce6c2e3e67
Motts182 <matias.diaz@globant.com>
2016-03-16 14:07:04 -0300

    Detalles técnicos La aplicacion solo funciona en el dispositivo movil.
    _Desarrollado en Unity3D
    _El Streaming solo funciona en la APK, no cuando se corre en Unity
    _Funciona en Android 4.1 o superior
    _El video es un Streaming de un externo consumido por un Asset
    _Desarrollada para Cardboard
    
    Desarrollado por Matias Diaz(matias.diaz@globant.com)

---
+++ Assets/Scripts/CardboardUIEvent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CardboardUIEvent : MonoBehaviour {
+    public Color colorHover;
+    public Color colorNatural;
+    Image imageScript;
+
+    void Awake() {
+        imageScript = GetComponent<Image>();
+    }
+
+    public void OnPointEnterEvent() {
+        imageScript.color = colorHover;
+    }
+
+    public void OnPointExitEvent()
+    {
+        imageScript.color = colorNatural;
+    }
+}
